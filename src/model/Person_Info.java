dae9ddf21d9be975571051dd0e2c0fc0960a24fc
Shraddha Mhetre <mhetre.s@northeastern.edu>
2021-09-25 14:30:33 -0400

    first commit

---
+++ src/model/Person_Info.java
@@ -0,0 +1,183 @@
+
+package model;
+
+/**
+ *
+ * @author mhetr
+ */
+public class Person_Info {
+    private String name;
+    private String geographic_Data;
+    private String dob;
+    private String telephone_Number;
+    private String fax_Number;
+    private String email_Address;
+    private String social_Security_Number;
+    private String medical_Record_Number;
+    private String helth_Plan_Benificiary_Number;
+    private String bank_acc_no;
+    private String licence_no;
+    private String vehicle_identifier;
+    private String device_identifiers;
+    private String LinkedIn;
+    private String ip_add;
+    private String biometric_id;
+    private String photo;
+    private String uniId;
+    private String text;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographic_Data() {
+        return geographic_Data;
+    }
+
+    public void setGeographic_Data(String geographic_Data) {
+        this.geographic_Data = geographic_Data;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTelephone_Number() {
+        return telephone_Number;
+    }
+
+    public void setTelephone_Number(String telephone_Number) {
+        this.telephone_Number = telephone_Number;
+    }
+
+    public String getFax_Number() {
+        return fax_Number;
+    }
+
+    public void setFax_Number(String fax_Number) {
+        this.fax_Number = fax_Number;
+    }
+
+    public String getEmail_Address() {
+        return email_Address;
+    }
+
+    public void setEmail_Address(String email_Address) {
+        this.email_Address = email_Address;
+    }
+
+    public String getSocial_Security_Number() {
+        return social_Security_Number;
+    }
+
+    public void setSocial_Security_Number(String social_Security_Number) {
+        this.social_Security_Number = social_Security_Number;
+    }
+
+    public String getMedical_Record_Number() {
+        return medical_Record_Number;
+    }
+
+    public void setMedical_Record_Number(String medical_Record_Number) {
+        this.medical_Record_Number = medical_Record_Number;
+    }
+
+    public String getHelth_Plan_Benificiary_Number() {
+        return helth_Plan_Benificiary_Number;
+    }
+
+    public void setHelth_Plan_Benificiary_Number(String helth_Plan_Benificiary_Number) {
+        this.helth_Plan_Benificiary_Number = helth_Plan_Benificiary_Number;
+    }
+
+    public String getBank_acc_no() {
+        return bank_acc_no;
+    }
+
+    public void setBank_acc_no(String bank_acc_no) {
+        this.bank_acc_no = bank_acc_no;
+    }
+
+    public String getLicence_no() {
+        return licence_no;
+    }
+
+    public void setLicence_no(String licence_no) {
+        this.licence_no = licence_no;
+    }
+
+    public String getVehicle_identifier() {
+        return vehicle_identifier;
+    }
+
+    public void setVehicle_identifier(String vehicle_identifier) {
+        this.vehicle_identifier = vehicle_identifier;
+    }
+
+    public String getDevice_identifiers() {
+        return device_identifiers;
+    }
+
+    public void setDevice_identifiers(String device_identifiers) {
+        this.device_identifiers = device_identifiers;
+    }
+
+    public String getLinkedIn() {
+        return LinkedIn;
+    }
+
+    public void setLinkedIn(String LinkedIn) {
+        this.LinkedIn = LinkedIn;
+    }
+
+    public String getIp_add() {
+        return ip_add;
+    }
+
+    public void setIp_add(String ip_add) {
+        this.ip_add = ip_add;
+    }
+
+    public String getBiometric_id() {
+        return biometric_id;
+    }
+
+    public void setBiometric_id(String biometric_id) {
+        this.biometric_id = biometric_id;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getUniId() {
+        return uniId;
+    }
+
+    public void setUniId(String uniId) {
+        this.uniId = uniId;
+    }
+
+    public String gettext() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text= text;
+    }
+    
+    
+    
+}
